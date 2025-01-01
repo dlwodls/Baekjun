956739bdb951336b395b2d3040351f450bf22f9b
Jaein Lee <jilee0818@gmail.com>
2025-01-02 00:23:52 +0900

    1330번

--- Baekjun/src/Bronze_V/Main.java
+++ Baekjun/src/Bronze_V/Main.java
@@ -2,3 +2,23 @@ package Bronze_V;
 
 import java.util.*;
 
+// 1330번. 두 수 비교하기
+public class Main {
+	public static void main(String[] args) {
+		double a, b;
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		
+		if (a > b) {
+			System.out.println(">");
+		}
+		else if (a == b) {
+			System.out.println("==");
+		}
+		else {
+			System.out.println("<");
+		}
+	}
+}
