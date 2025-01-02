9cf5dc22496d1cff730c89422bc3b40a02b9213e
Jaein Lee <jilee0818@gmail.com>
2025-01-03 01:33:39 +0900

    2475번

--- Baekjun/src/Bronze_V/Main.java
+++ Baekjun/src/Bronze_V/Main.java
@@ -1,22 +1,9 @@
 package Bronze_V;
 
-import java.math.BigInteger;
 import java.util.*;
 
-// 2438번. 별 찍기 - 1
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (i >= j) {
-					System.out.print("* ");
-				}
-			}
-			System.out.println();
-		}
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
