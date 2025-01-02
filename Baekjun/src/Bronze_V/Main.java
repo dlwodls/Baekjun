d0f89d04464004d91572347e3fa3898326c5e3ae
Jaein Lee <jilee0818@gmail.com>
2025-01-03 01:42:00 +0900

    2739번

--- Baekjun/src/Bronze_V/Main.java
+++ Baekjun/src/Bronze_V/Main.java
@@ -2,8 +2,15 @@ package Bronze_V;
 
 import java.util.*;
 
+// 2739번. 구구단
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int dan = sc.nextInt();
+		
+		for (int is = 1; is <= 9; is++) {
+			System.out.println(dan + " * " + is + " = " + (dan * is));
+		}
+	}
 }
